b56b422e8b8a7b0b0a3d69c92b4c3ae6138c512d
Zpdmain04 <awesome.fffyyy@yandex.ru>
2024-06-03 09:22:45 +0600

    updated

--- program.ts
+++ program.ts
@@ -2,6 +2,7 @@ import * as fs from 'fs';
 import * as yaml from 'js-yaml';
 import { MongoClient } from 'mongodb';
 import * as xlsx from 'xlsx';
+import {Command} from 'commander';
 
 class Student {
   name: string;
@@ -73,6 +74,7 @@ class MongoDB {
       const origdoc = await collection.findOne({name: item.name});
       if (!origdoc) {
         await collection.insertOne(item);
+        continue
       }
       const updoc = { ...origdoc, ...item };
       if (JSON.stringify(origdoc) === JSON.stringify(updoc)) {
@@ -86,6 +88,7 @@ class MongoDB {
     const collection = this.db.collection(collname)
     return await collection.find({}).toArray()
   }
+  
 }
 
 function createxcel(data: any[], filename: string) {
@@ -95,35 +98,91 @@ function createxcel(data: any[], filename: string) {
   xlsx.writeFile(wb, `${filename}.xlsx`)
 }
 
-async function main() {
+async function loadStudents() {
   try {
+    if (!fs.existsSync('students.yaml')) {
+      throw new Error("Файл 'students.yaml' не найден.");
+    }
     const MONGO = new MongoDB('mongodb://root:example@127.0.0.1:27017/', 'Students_n_Disciplines')
-
     await MONGO.connect()
-
     const studentsData = yaml.load(fs.readFileSync('students.yaml', 'utf-8')) as Student[]
     await MONGO.saveData('Students', studentsData)
-    const disciplinesData = yaml.load(fs.readFileSync('disciplines.yaml', 'utf-8')) as Discipline[]
-    await MONGO.saveData('Disciplines', disciplinesData)
-    const students = await MONGO.getData('Students')
-    const disciplines = await MONGO.getData('Disciplines')
-    const studentDisciplinesData: any[][] = []
-    for (const student of students) {
-      const studentDisciplines = student.disciplines.map((disciplineName: any) => {
-        const discipline = disciplines.find(d => d.name === disciplineName)
-        return discipline ? discipline.name : null;
-      })
-      studentDisciplinesData.push([student.name, ...studentDisciplines])
-    }
-    createxcel(studentDisciplinesData, 'Students')
-    const disciplineAssignments = disciplines.map(discipline => [
-      discipline.name,
-      ...discipline.assignments.map((assignment: { name: any; }) => assignment.name),
-    ]);
-    createxcel(disciplineAssignments, 'Disciplines')
+    console.log('Студенты успешно загружены в MongoDB.')
     await MONGO.disconnect()
   } catch (e) {
     console.error(e)
   }
 }
-main()
+
+async function loadDisciplines() {
+  try {
+    if (!fs.existsSync('disciplines.yaml')) {
+      throw new Error("Файл 'disciplines.yaml' не найден.");
+    }
+    const MONGO = new MongoDB('mongodb://root:example@127.0.0.1:27017/', 'Students_n_Disciplines')
+    await MONGO.connect()
+    const disciplinesData = yaml.load(fs.readFileSync('disciplines.yaml', 'utf-8')) as Discipline[]
+    await MONGO.saveData('Disciplines', disciplinesData)
+    console.log('Дисциплины успешно загружены в MongoDB.')
+    await MONGO.disconnect()
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+
+async function exportStudents() {
+  const MONGO = new MongoDB('mongodb://root:example@127.0.0.1:27017/', 'Students_n_Disciplines')
+  await MONGO.connect()
+  const students = await MONGO.getData('Students')
+  const disciplines = await MONGO.getData('Disciplines')
+  const studentDisciplinesData: any[][] = []
+  for (const student of students) {
+    const studentDisciplines = student.disciplines.map((disciplineName: any) => {
+      const discipline = disciplines.find(d => d.name === disciplineName)
+      return discipline ? discipline.name : null;
+    })
+    studentDisciplinesData.push([student.name, ...studentDisciplines])
+  }
+  createxcel(studentDisciplinesData, 'Students')
+  console.log('Данные о студентах успешно выгружены в файл Students.xlsx.')
+  await MONGO.disconnect()
+}
+
+async function exportDisciplines() {
+  const MONGO = new MongoDB('mongodb://root:example@127.0.0.1:27017/', 'Students_n_Disciplines')
+  await MONGO.connect()
+  const disciplines = await MONGO.getData('Disciplines')
+  const disciplineAssignments = disciplines.map(discipline => [
+    discipline.name,
+    ...discipline.assignments.map((assignment: { name: any; }) => assignment.name),
+  ]);
+  createxcel(disciplineAssignments, 'Disciplines')
+  console.log('Данные о дисциплинах успешно выгружены в файл Disciplines.xlsx.')
+  await MONGO.disconnect()
+}
+const program = new Command()
+
+
+program
+  .command('ls')
+  .description('Загрузить данные студентов из YAML файла в MongoDB')
+  .action(loadStudents)
+
+program
+  .command('ld')
+  .description('Загрузить данные дисциплин из YAML файла в MongoDB')
+  .action(loadDisciplines)
+
+program
+  .command('es')
+  .description('Экспортировать данные о студентах в Excel файл')
+  .action(exportStudents)
+
+program
+  .command('ed')
+  .description('Экспортировать данные о дисциплинах в Excel файл')
+  .action(exportDisciplines)
+
+program.parse(process.argv)
+
